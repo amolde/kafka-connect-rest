d14f7423ec32595baa43730e8dd275ecc41b5042
Amol Deshmukh <a.deshmukh@northeastern.edu>
2020-01-28 22:26:00 -0500

    eclipse setting added for reference

--- runme.sh
+++ runme.sh
@@ -2,7 +2,7 @@ jenv global
 # 1.8.0.232
 jenv version
 # 1.8.0.232 (set by /Users/adeshmukh/.jenv/version)
-mvn clean install -DskipTests
+mvn clean install
 cp kafka-connect-rest-plugin/target/kafka-connect-rest-plugin-1.0.3-shaded.jar ~/ckg/cp-kafka-connect/jars/.
 cp kafka-connect-transform-from-json/kafka-connect-transform-from-json-plugin/target/kafka-connect-transform-from-json-plugin-1.0.3-shaded.jar ~/ckg/cp-kafka-connect/jars/.
 cp kafka-connect-transform-add-headers/target/kafka-connect-transform-add-headers-1.0.3-shaded.jar ~/ckg/cp-kafka-connect/jars/.
